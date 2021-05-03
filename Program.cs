929634b767b6309da332c1614eb9b6e1b87ac843
zaylon1 <83616303+zaylon1@users.noreply.github.com>
2021-05-03 20:09:31 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicios_Propostos_P2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = -10;
+
+            Console.WriteLine("N NEGATIVO");
+
+            if (x < -8 ) {
+                Console.WriteLine("NEGATIVO");
+            }
+
+            
+
+            
+        }
+    }
+}
